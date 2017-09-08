3527456eca9398a42039c4bee467ba22a771db49
Jennifer Pazos <jmp521@gmail.com>
2017-09-07 21:43:05 -0500

    got react-bootstrap working and got the navbar component to render

---
+++ group-management-client/src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+import Navigation from './Nav'
+
+class App extends Component{
+  render() {
+
+    return(
+
+      <div>
+        <Navigation />
+      </div>
+    )
+
+  }
+
+}
+
+export default App;
