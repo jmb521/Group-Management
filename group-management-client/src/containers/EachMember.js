41bd8a84e46317616b4e7e6d92c35dff747391b9
Jennifer Pazos <jmp521@gmail.com>
2017-10-17 14:11:37 -0500

    changed EachMember to a class

--- group-management-client/src/containers/EachMember.js
+++ group-management-client/src/containers/EachMember.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button} from 'react-bootstrap'
 
+class EachMember extends Component {
+  constructor() {
+    super()
 
-const EachMember = (props) => (
-<tbody>
-  <tr>
-    <td>{props.member.membership_status.member_number}</td>
-    <td>{props.member.username}</td>
-    <td>{props.member.user_info.first_name}</td>
-    <td>{props.member.user_info.last_name}</td>
-    <td>{props.member.membership_status.is_member + " "}</td>
-    <td>
-    <Button bsStyle="success" >Renew</Button>
-    <Button bsStyle="danger" >Remove</Button>
-    </td>
+  }
+  render() {
+    return(
+      <tbody>
+      <tr>
+      <td>{this.props.member.membership_status.member_number}</td>
+      <td>{this.props.member.username}</td>
+      <td>{this.props.member.user_info.first_name}</td>
+      <td>{this.props.member.user_info.last_name}</td>
+      <td>{this.props.member.membership_status.is_member + " "}</td>
+      <td>
+      <Button bsStyle="success" >Renew</Button>
+      <Button bsStyle="danger" >Remove</Button>
+      </td>
 
-    </tr>
-  </tbody>
-)
+      </tr>
+      </tbody>
+
+
+
+
+    )
+  }
+}
+
+
 
 
 export default EachMember
