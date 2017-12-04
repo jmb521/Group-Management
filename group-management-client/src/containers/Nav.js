364f2eff5059d05fb51a8370932b633ca5077918
Jennifer Pazos <jmp521@gmail.com>
2017-12-04 15:44:47 -0600

    fixed error in navigation

--- group-management-client/src/containers/Nav.js
+++ group-management-client/src/containers/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
@@ -22,14 +22,14 @@ class Navigation extends React.Component{
       <NavDropdown eventKey={2} title="Club" id="basic-nav-dropdown">
         <MenuItem eventKey={2.1} href="/addclub">Add Club</MenuItem>
         </NavDropdown>
-        <MenuItem href="/addmember">Add Member</MenuItem>
-      <NavDropdown eventKey={3} title="Membership Management" id="basic-nav-dropdown">
+        <NavItem eventKey={3} href="/addmember">Add Member</NavItem>
+      <NavDropdown eventKey={4} title="Membership Management" id="basic-nav-dropdown">
 
-          <MenuItem eventKey={3.1} href="/membershipmanagement">Membership</MenuItem>
+          <MenuItem eventKey={4.1} href="/membershipmanagement">Membership</MenuItem>
           <MenuItem divider />
-          <MenuItem eventKey={3.2} href="/membershipmanagement/pendingmembers">Pending Members</MenuItem>
-          <MenuItem eventKey={3.3} href='/membershipmanagement/memberrenewal'>Member Renewal</MenuItem>
-          <MenuItem eventKey={3.4} href="/membershipmanagement/removedmembers">Removed Members</MenuItem>
+          <MenuItem eventKey={4.2} href="/membershipmanagement/pendingmembers">Pending Members</MenuItem>
+          <MenuItem eventKey={4.3} href='/membershipmanagement/memberrenewal'>Member Renewal</MenuItem>
+          <MenuItem eventKey={4.4} href="/membershipmanagement/removedmembers">Removed Members</MenuItem>
 
         </NavDropdown>
 
