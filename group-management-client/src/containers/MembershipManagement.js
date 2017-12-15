4655ce27aef512bb51a54d702013a4fc0698a84d
Jennifer Pazos <jmp521@gmail.com>
2017-12-15 12:21:18 -0600

    converted to a stateless component

--- group-management-client/src/containers/MembershipManagement.js
+++ group-management-client/src/containers/MembershipManagement.js
@@ -3,37 +3,39 @@
 import React, { Component } from 'react';
 import {MemberCount} from './MemberCount'
 import {PendingMemberCount} from './PendingMemberCount'
+// import {bindActionCreators} from 'redux'
 // import EachMember from './EachMember'
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 import { Grid, Row, Col} from 'react-bootstrap'
 // import { getMembers } from '../actions/membership'
 // import { bindActionCreators } from 'redux'
-
+import membershipdashboardreducer from '../reducers/membershipdashboardreducer'
+import membersreducer from '../reducers/membersreducer'
 // import { updateMemberStatus } from '../actions/membership'
 // import {removeMember } from '../actions/membership'
-class MembershipManagement extends Component {
-  state = {
-    memberCount: "1,543",
-    pendingMemberCount: 42,
-
-  }
+import {GetClubs} from './GetClubs'
+import store from '../store.js'
+export const MembershipManagement = function(props) {
 
 
-  render() {
+
+    const pendingmembercount = props.members.filter((m) => m.membership_status.is_member === "pending").length
+    const membercount = props.members.length
     return(
       <div>
         <Grid className="membershipmanagement">
           <Row>
             <Col sm={6} md={12} className="dashboardheader">
               <h1>Membership Dashboard</h1>
+              <GetClubs clubs={props.clubs} handleOnChange={props.handleOnChange}/>
             </Col>
           </Row>
           <Row>
             <Col sm={6} md={3} className="member_count">
-              <MemberCount memberCount={this.state.memberCount} />
+              <MemberCount memberCount={membercount} />
             </Col>
             <Col sm={6} md={3} className="pending_member_count">
-              <PendingMemberCount pendingMemberCount={this.state.pendingMemberCount} />
+              <PendingMemberCount pendingMemberCount={pendingmembercount} />
             </Col>
             <Col sm={6} md={3}>
 
@@ -44,7 +46,5 @@ class MembershipManagement extends Component {
         </Grid>
       </div>
     )
-  }
-}
 
-export default MembershipManagement
+}
