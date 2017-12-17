663e9537114a8917fe2328dd22c4b9d73936dfb2
Jennifer Pazos <jmp521@gmail.com>
2017-12-17 00:59:49 -0600

    removed sample functions

--- group-management-client/src/actions/membership.js
+++ group-management-client/src/actions/membership.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch'
 const setMembers = members => {
   return {
     type: 'GET_MEMBERS_SUCCESS',
-    
+
     members
 
     }
@@ -43,33 +43,4 @@ export const removeMember = (member) => {
     .then(user => dispatch(updateMemberStatus({membership_status: {"is_member": "not a member"}})))
     .then(error => console.log(error))
   }
-}
-
-
-//export const removeMember = (user) => {
-
-  // const formData = new FormData();
-  // formData.append(user.membership_status.is_member, "not a member")
-  // return fetch(`http://localhost:3001/api/clubs/${user.club_id}/users/${user.id}`, {
-  //     method: "PUT",
-  //     body: JSON.stringify({user:formData}),
-  //   })
-  //   console.log(formData)
-  //   .then(response => response.json())
-  //   // .then(member_status => debugger )
-  //   .then(error => console.log(error))
-  // }
-
-
-
-function createNewProfile(profile) {
-    const formData = new FormData();
-    formData.append('first_name', profile.firstName);
-    formData.append('last_name', profile.lastName);
-    formData.append('email', profile.email);
-    return fetch('http://example.com/api/v1/registration', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
 }
