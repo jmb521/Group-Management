ca9b1ef4ac46228ba167316d5c3272e1d87e750d
Jennifer Pazos <jmp521@gmail.com>
2018-01-08 22:31:16 -0600

    added Kids component with ability to add and remove component

--- group-management-client/src/containers/ContactInfo.js
+++ group-management-client/src/containers/ContactInfo.js
@@ -5,15 +5,43 @@ import {bindActionCreators} from 'redux'
 import store from '../store.js'
 import {Kids} from './Kids'
 import {Family} from './Family'
+import { Grid, Row, Col, Form, Panel, Button} from 'react-bootstrap'
 
 class ContactInfo extends Component {
-  //use the member id from the newly created member
+  constructor() {
+    super()
+    this.state = {
+      KidCount: 1,
+    }
 
-  render() {
+  }
+  addKid = () => {
+    this.setState({
+      KidCount: this.state.KidCount + 1,
+
+    })
+
+  }
+  handleRemoveClick = (id) => {
+    this.setState({
+      KidCount: this.state.KidCount -1,
+    })
+  }
+
+render() {
+
+    const Kidcomponent = [];
+
+    for(var i=0; i< this.state.KidCount; i++) {
+      Kidcomponent.push(<Panel key={i}><Kids key={i} id={i} onRemoveClick={this.handleRemoveClick} /></Panel>)
+    }
+    console.log("$%", Kidcomponent)
+
 
     return (
-
+      <Grid>
       <div>
+        <Form onSubmit={this.handleOnSubmit}>
         <div>
           <UserContactInfo />
         </div>
@@ -21,10 +49,14 @@ class ContactInfo extends Component {
           <Family />
         </div>
         <div>
-          <Kids />
-        </div>
+        <Button onClick={this.addKid}> Add Kid</Button>
 
+          {Kidcomponent}
+
+        </div>
+        </Form>
       </div>
+      </Grid>
 
     )
   }
--- group-management-client/src/containers/Kids.js
+++ group-management-client/src/containers/Kids.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Row, Col, Button} from 'react-bootstrap'
 
 
 export const Kids = (props) => {
 
     return(
       <div>
-      User Kids
+      <p>Here is the kids component</p>
+      <Button onClick={() => {props.onRemoveClick(props.id)}}>Remove</Button>
       </div>
     )
   }
