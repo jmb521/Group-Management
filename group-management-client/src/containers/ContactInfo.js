6df998dd80ba77f73d571ae759f73f3652a8c09d
Jennifer Pazos <jmp521@gmail.com>
2018-01-09 01:35:10 -0600

    added datepicker

--- group-management-client/src/containers/ContactInfo.js
+++ group-management-client/src/containers/ContactInfo.js
@@ -7,14 +7,18 @@ import {Kids} from './Kids'
 import {Family} from './Family'
 import { Grid, Row, Col, Form, Panel, Button} from 'react-bootstrap'
 
+
 class ContactInfo extends Component {
   constructor() {
     super()
     this.state = {
       KidCount: 1,
+
     }
 
   }
+
+
   addKid = () => {
     this.setState({
       KidCount: this.state.KidCount + 1,
--- group-management-client/src/containers/Kids.js
+++ group-management-client/src/containers/Kids.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import { Row, Col, Button} from 'react-bootstrap'
-
-
+import FieldGroup from '../fieldgroup.js'
+var DatePicker = require("react-bootstrap-date-picker");
 export const Kids = (props) => {
 
     return(
       <div>
-      <p>Here is the kids component</p>
+      <Row>
+        <Col xs={12} md={8}>
+        <FieldGroup
+             id="kid_name"
+             type="text"
+             label="Kid Name"
+             placeholder="Enter your child's first and last name"
+             name="kid_name"
+             onChange={props.handleOnChange}
+            //  value={first_name}
+         />
+
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={8}>
+        <DatePicker
+        label="Kid Birthday"
+        id="kid_birthday"
+        onChange={props.handleOnChange}
+        />
+        </Col>
+      </Row>
       <Button onClick={() => {props.onRemoveClick(props.id)}}>Remove</Button>
       </div>
     )
