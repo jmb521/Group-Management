60f62f70dfd34442c7c704968e081f5b5a1d35ca
Jennifer Pazos <jmp521@gmail.com>
2018-01-11 14:33:23 -0600

    changed the way a member was concatonated to the array

--- group-management-client/src/reducers/membersreducer.js
+++ group-management-client/src/reducers/membersreducer.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
   switch(action.type) {
     case 'CREATE_MEMBER_SUCCESS':
-      return state.concat({members: action.members});
+      return state.concat(action.members);
     case 'GET_MEMBERLIST_SUCCESS':
       return action.members;
     case 'GET_MEMBERS_SUCCESS':
