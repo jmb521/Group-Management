100d41c0c522aba777f016384b7df1d7377d2729
Jennifer Pazos <jmp521@gmail.com>
2018-01-15 19:17:52 -0600

    changed all the instances variables to make sure they were the same

--- group-management-api/app/controllers/api/user_families_controller.rb
+++ group-management-api/app/controllers/api/user_families_controller.rb
@@ -1,16 +1,20 @@
-class Api::UserFamiliesController < ApplicationController
-  before_action :set_user_families, only: [:show, :update, :destroy]
+  require 'pry'
 
+class Api::UserFamiliesController < ApplicationController
+  before_action :set_user_family
+  before_action :set_user, only: [:index, :show, :update]
   def show
-    render json: @user_families
+    @user_family = @user.user_family
+    render json: @user_family
   end
 
   def create
-    @user_families = UserFamilies.new(user_family_params)
-    if @user_families.save
-      render json: @user_families
+    @user_family = UserFamily.new(user_family_params)
+
+    if @user_family.save
+      render json: @user_family
     else
-      render json: { message: @user_families.errors }, status: 400
+      render json: { message: @user_family.errors }, status: 400
     end
   end
 
@@ -18,27 +22,32 @@ class Api::UserFamiliesController < ApplicationController
   end
 
   def update
-    if @user_families.update(user_family_params)
-      render json: @user_families
+    
+    if @user_family.update(user_family_params)
+      render json: @user_family
     else
-      render json: { message: @user_families.errors }, status: 400
+      render json: { message: @user_family.errors }, status: 400
     end
   end
 
   def destroy
-    if @user_families.destroy
+    if @user_family.destroy
       render status: 204
     end
   end
 
   private
 
-  def set_user_families
-    @user_families = UserFamilies.find(params[:id])
+  def set_user_family
+    @user_family = UserFamily.find(params[:id])
+  end
+  def set_user
+    @user = User.find_by(:id => params[:user_id])
   end
 
+
   def user_family_params
-    params.require(:user_families).permit(:user_id, :user_birthday, :spouse, :spouse_birthday)
+    params.require(:user_family).permit(:user_id, :user_birthday, :spouse, :spouse_birthday)
   end
 
 end
